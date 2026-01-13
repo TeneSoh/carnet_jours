b35394b3bcca61d1e9925595edbb5bea17c2dbfb
Loic Baker <loicsoh00@gmail.com>
2026-01-13 14:12:08 +0100

    ajouter du style a la page contact

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-   path('', views.index, name='contacts'),
+   path(' ', views.index, name='contacts'),
    path('creer-contact/', views.store, name='store'),
    path('edite-contact/<int:id>', views.edite, name='edite'),
    path('show-contact/<int:id>', views.show, name='show'),
